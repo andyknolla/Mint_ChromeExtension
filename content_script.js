7f27d3537c16ed8588a36c19688fe48a14069dcf
Andy Knolla <andyknolla@gmail.com>
2018-02-26 20:16:39 -0700

    Basic functionality works

--- content_script.js
+++ content_script.js
@@ -1,10 +1,44 @@
-console.log('content script, yay!', document);
 
-// document.addEventListener('click', () => {
+let table = document.getElementById('transaction-list-body');
+let transactions = table.getElementsByClassName('firstdate');
 
-  var table = document.getElementById('transaction-list');
-  var prices = document.querySelectorAll('.money');
+let unique = [];
+unique.push(transactions[0]);
 
-// .innerHTML or innerText have the dollar value.
+let duplicates = [];
 
-  debugger;
+for(var i = 1; i < transactions.length; i++) {
+  let date = transactions[i].getElementsByClassName('date')[0].innerText;
+  let price = transactions[i].getElementsByClassName('money')[0].innerText;
+
+  let isDupe = false;
+
+  for(var j = 0; j < unique.length; j++) {
+    let dupePrice = unique[j].getElementsByClassName('money')[0].innerText;
+    let dupeDate = unique[j].getElementsByClassName('date')[0].innerText;
+    if( price != '$0.00' && price === dupePrice && date === dupeDate ) {
+      // change background color of dupePrice row
+      unique[j].style.background = 'bisque';
+      isDupe = true;
+      break
+    }
+  }
+
+  isDupe ? duplicates.push(transactions[i]) : unique.push(transactions[i]);
+
+}
+
+highlightDupes(duplicates);
+
+// go through duplicates, change background color for each
+
+function highlightDupes(arr) {
+  arr.map( (element) => {
+    element.style.background = 'bisque';
+  })
+}
+
+let duplicateAmounts = duplicates.map( (element) => {
+    return element.getElementsByClassName('money')[0].innerText;
+})
+debugger
--- popup.js
+++ popup.js
@@ -1,16 +1,4 @@
 
-function tryStuff() {
-  console.log('popup.js')
-
-}
-
 document.addEventListener('DOMContentLoaded', () => {
-  tryStuff();
-  console.log('browserAction');
   chrome.tabs.executeScript(null, { file: 'content_script.js' });
 });
-
-// document.addEventListener('click', () => {
-//   console.log('browserAction');
-//   chrome.tabs.executeScript(null, { file: 'content-script.js' });
-// });
